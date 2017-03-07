10a1162b5f3c884f83e856b04c66771e70fd456b
Jon Guan <jonathan.guan@capitalone.com>
2017-03-06 16:42:39 -0800

    java docs

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -10,6 +10,11 @@ public class Solution {
     public Solution(){
     }
 
+    /**
+     * Takes in an array of strings, and returns an array of answers
+     * @param s - string input array
+     * @return - array of int answers of how many powers of five fit into each string in the array
+     */
     public int[] getMin(String[] s){
 
     }
